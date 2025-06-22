9f537b5b2d7559a77f52cd7fdb14915f59a8a9b2
Leandro Pintos <leandropintosn97@gmail.com>
2025-06-22 14:50:40 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,14 +1,14 @@
-#include "interfaz.h"    // Para mostrarMenu, reservarTurno, listarTurnos, cancelarTurno, reagendarTurno
-#include "turnos.h"      // Para cargarTurnosDesdeArchivo, guardarTurnosEnArchivo, liberarListaTurnos
-#include "validaciones.h" // Para getValidatedIntInput (usado en main)
-#include <stdio.h>       // Para printf
+#include "interfaz.h"    
+#include "turnos.h"      
+#include "validaciones.h" 
+#include <stdio.h>       
 
 int main() {
     cargarTurnosDesdeArchivo();
     int opcion;
     do {
         mostrarMenu();
-        opcion = getValidatedIntInput("Seleccione una opcion: ", 0, 4); // El rango es de 0 a 4
+        opcion = getValidatedIntInput("Seleccione una opcion: ", 0, 4); 
 
         switch(opcion) {
             case 1: reservarTurno(); break;
@@ -16,10 +16,10 @@ int main() {
             case 3: cancelarTurno(); break;
             case 4: reagendarTurno(); break;
             case 0: guardarTurnosEnArchivo(); printf("Saliendo...\n"); break;
-            // No necesitamos un default para "Opcion invalida" aqui porque getValidatedIntInput ya lo maneja
+          
         }
     } while(opcion != 0);
 
-    liberarListaTurnos(); // Asegurarse de liberar la memoria antes de salir
+    liberarListaTurnos();
     return 0;
 }
